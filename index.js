c969d5256b01430be9d098d148bbe523af508345
MatiasR99 <150948239+MatiasR99@users.noreply.github.com>
2024-03-01 19:07:24 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let contador1 = 0;
+let contador2 = 0;
+let resta = 0;
+const sumar = {
+    sumar1() {
+      contador1 = contador1 + 1;
+      console.log( contador1);
+    
+}
+ }
+ const sumar2 = {
+    sumar2() {
+      contador2 = contador2 + 2;
+      console.log( contador2);
+    
+}
+ }
+ const resta1 = {
+    resta1() {
+      resta = resta - 1;
+      console.log( resta);
+    
+}
+ }
+
